369184c77c4466857144823bfde0840c68d820f8
MingiPedeKuskilEestis <mihkelorg.mo@gmail.com>
2018-12-14 22:22:09 +0200

    nupud titlescreenil

--- titlescreen.py
+++ titlescreen.py
@@ -1,4 +1,5 @@
 import pygame, sys
+screen = pygame.display.set_mode([1280,720])
 
 def titlescreen(screen):
     screen.fill([255, 255, 255])
@@ -8,6 +9,8 @@ def titlescreen(screen):
     sekritfont = pygame.font.SysFont('Comic Sans MS', 15)
     titletext = font.render('Bazzer Royalty', False, (0, 0, 0))
     sekrit = sekritfont.render('dab', False, (0, 0, 0))
+    hosttext = font.render('Host', False, (0, 0, 0))
+    clienttext = font.render('Client', False, (0, 0, 0))
     screen.blit(titletext, (0, 0))
     while True:
         mousepos = pygame.mouse.get_pos()
@@ -22,11 +25,19 @@ def titlescreen(screen):
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        if mousepress == (1, 0, 0) and 250 > mousepos[0] > 50 and 200 > mousepos[0] > 100:
+        if mousepress == (1, 0, 0) and 250 > mousepos[0] > 50 and 200 > mousepos[1] > 100:
             pygame.draw.rect(screen, (0, 200, 0), (50, 100, 200, 100))
+            screen.blit(clienttext, (82, 110))
         else:
             pygame.draw.rect(screen, (0, 255, 0), (50, 100, 200, 100))
-
+            screen.blit(clienttext, (82, 110))
+        if mousepress == (1, 0, 0) and 500 > mousepos[0] > 300 and 300 > mousepos[1] > 100:
+            pygame.draw.rect(screen, (0, 200, 0), (300, 100, 200, 100))
+            screen.blit(hosttext, (342, 110))
+        else:
+            pygame.draw.rect(screen, (0, 255, 0), (300, 100, 200, 100))
+            screen.blit(hosttext, (342, 110))
         if mousepress == (1, 1, 1):
             screen.blit(sekrit, (770,580))
         pygame.display.flip()
+titlescreen(screen)
