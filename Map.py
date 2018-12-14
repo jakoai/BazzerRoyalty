73b19eb3152f2a31a566b8ee1ed97170c83f2450
UltraDragonDreamFeet <uglysanta19@gmail.com>
2018-12-14 18:20:00 +0200

    map

--- Map.py
+++ Map.py
@@ -16,23 +16,37 @@ class map():
     def generatemap(self):
         for ny,item in enumerate(self.map):
             for nx,i in enumerate(item):
-                x = random.randint(0,1000)
-                if x <= 20:
-                    self.map[ny][nx] = 1
+                x = random.randint(0,3000)
 
-                    if x <= 10:
-                        
-                        for cx in range(5):
-                            if nx+cx < self.mapx:
-                                for cy in range(5):
-                                    if ny+cy < self.mapy:
-                                        z = random.randint(0, 4)
-                                        if z <= 3:
-                                            self.map[ny+cy][nx+cx] = 1
+                if x <= 30:
+                    for cx in range(random.randint(1,5)):
+                        if nx + cx < self.mapx:
+                            for cy in range(random.randint(1,5)):
+                                if ny + cy < self.mapy:
+                                    z = random.randint(0, 6)
+                                    if z <= 2:
+                                        self.map[ny + cy][nx + cx] = 1
+                x = random.randint(0,3000)
+
+                if x <= 3:
+                    for cx in range(random.randint(5,8)):
+                        if nx+cx < self.mapx:
+                            for cy in range(random.randint(5,8)):
+                                if ny+cy < self.mapy:
+                                    z = random.randint(0, 6)
+                                    if z <= 2:
+                                        self.map[ny+cy][nx+cx] = 1
+                x = random.randint(0,3000)
+                if x <=5:
+                    self.map[ny][nx] = 2
 
 
-    def draw(self, screen, color, tile):
+
+
+    def draw(self, screen, tile, charx, chary):
         for ny,item in enumerate(self.map):
             for nx,i in enumerate(item):
                 if i == 1:
-                    pygame.draw.rect(screen, color, (nx*tile,ny*tile , tile, tile))
+                    pygame.draw.rect(screen, (0,255,255), (nx*tile-charx,ny*tile-chary , tile, tile))
+                if i == 2:
+                    pygame.draw.rect(screen, (0,0,255), (nx*tile-charx,ny*tile-chary , tile, tile))
