75ddb4fa2d06533d7601fbceac34bac05e41eadf
MingiPedeKuskilEestis <mihkelorg.mo@gmail.com>
2018-12-14 20:45:27 +0200

    nupp titlescreenil

--- titlescreen.py
+++ titlescreen.py
@@ -1,4 +1,5 @@
 import pygame, sys
+screen = pygame.display.set_mode([1280,720])
 
 def titlescreen(screen):
     screen.fill([255, 255, 255])
@@ -12,6 +13,7 @@ def titlescreen(screen):
     while True:
         mousepos = pygame.mouse.get_pos()
         mousepress = pygame.mouse.get_pressed()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -21,15 +23,11 @@ def titlescreen(screen):
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        if mousepress == (1, 0, 0):
-            pygame.draw.rect(screen, [255, 0, 0], (100,100,50,50))
-        elif mousepress == (0, 1, 0):
-            pygame.draw.rect(screen, [0, 255, 0], (100, 100, 50, 50))
-        elif mousepress == (0, 0, 1):
-            pygame.draw.rect(screen, [0, 0, 255], (100, 100, 50, 50))
-        if mousepress == (0, 0, 0):
-            pygame.draw.rect(screen, [255, 255, 255], (100, 100, 50, 50))
+        if mousepress == (1, 0, 0) and 250 > mousepos[0] > 50 and 200 > mousepos[0] > 100:
+            pygame.draw.rect(screen, (0, 200, 0), (50, 100, 200, 100))
+        else:
+            pygame.draw.rect(screen, (0, 255, 0), (50, 100, 200, 100))
+
         if mousepress == (1, 1, 1):
             screen.blit(sekrit, (770,580))
-
         pygame.display.flip()
