4a09c6b02713ee9ffc25a37cdb64bf95d6275f0e
UltraDragonDreamFeet <uglysanta19@gmail.com>
2018-12-14 21:42:41 +0200

    opmap

--- Map.py
+++ Map.py
@@ -1,4 +1,4 @@
-import random, pygame
+import random, pygame, pygame.gfxdraw
 
 class map():
     def __init__(self, mapx, mapy):
@@ -16,69 +16,55 @@ class map():
     def generatemap(self):
         for ny,item in enumerate(self.map):
             for nx,i in enumerate(item):
-                x = random.randint(0,10)
+                x = random.randint(0,1000)
                 wall=0
                 empty=0
 
-                if x <= 30:
+                if x <= 500:
                     for cx in range(random.randint(1,2)):
                         if nx + cx < self.mapx:
-                            for cy in range(random.randint(1,2)):
+                            for cy in range(random.randint(1,1)):
                                 if ny + cy < self.mapy:
                                     z = random.randint(0, 6)
                                     if z <= 2:
                                         self.map[ny + cy][nx + cx] = 1
-                x = random.randint(0,3000)
-
-                if x <= 3:
-                    for cx in range(random.randint(5,8)):
+                x = random.randint(0,1000)
+                if x <= 2:
+                    for cx in range(random.randint(5,20)):
                         if nx+cx < self.mapx:
-                            for cy in range(random.randint(5,8)):
+                            for cy in range(random.randint(5,20)):
                                 if ny+cy < self.mapy:
                                     z = random.randint(0, 6)
-                                    if z <= 2:
+                                    if z <= 3:
                                         self.map[ny+cy][nx+cx] = 1
-                #Lauri sitt
-                if x <=1:
-                    self.map[ny][nx] = 1
 
-                for k in range(-1, 2):
-                    for l in range(-1, 2):
-                        try:
-                            if self.map[ny + k][nx + l] == 0 and self.map[ny][nx] == 1:
-                                empty += 1
-                            elif self.map[ny + k][nx + l] == 1 and self.map[ny][nx] == 0:
-                                wall += 1
-                        except:
-                            pass
+                for counter in range(7):
+                    for k in range(-1, 2):
+                        for l in range(-1, 2):
+                            try:
+                                if self.map[ny + k][nx + l] == 0 and self.map[ny][nx] == 1:
+                                    empty += 1
+                                elif self.map[ny + k][nx + l] == 1 and self.map[ny][nx] == 0:
+                                    wall += 1
+                            except:
+                                pass
 
-                if self.map[ny][nx] == 1 and empty > wall:
-                    self.map[ny][nx] = 0
-                    """
-                    home = 0
-                    while home == 0:
-                        newhomex = random.randint(0, self.mapx - 1)
-                        newhomey = random.randint(0, self.mapy - 1)
-                        if self.map[newhomex][newhomey] == 0:
-                            self.map[newhomex][newhomey] = 1
-                            home = 1
-                            """
+                    if self.map[ny][nx] == 1 and empty > wall:
+                        self.map[ny][nx] = 0
 
+                    elif self.map[ny][nx] == 0 and wall > empty:
+                        self.map[ny][nx] = 1
 
-                elif self.map[ny][nx] == 0 and wall > empty:
-                    self.map[ny][nx] = 1
-
-                x = random.randint(0,3000)
+                x = random.randint(0,1000)
                 if x <=5:
                     self.map[ny][nx] = 2
-            print(item)
 
     def draw(self, screen, tile, charx, chary):
         for ny,item in enumerate(self.map):
             for nx,i in enumerate(item):
                 if i == 1:
-                    pygame.draw.rect(screen, (0,255,255), (nx*tile-charx,ny*tile-chary , tile, tile))
+                    pygame.gfxdraw.box(screen, (nx*tile-charx,ny*tile-chary , tile, tile), (0,255,255))
                 if i == 2:
-                    pygame.draw.rect(screen, (0,0,255), (nx*tile-charx,ny*tile-chary , tile, tile))
+                    pygame.gfxdraw.box(screen, (nx*tile-charx,ny*tile-chary , tile, tile), (0,0,255))
 
 
