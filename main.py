f98e1c03bff8d9b82c355f71df2b008ea7856d3e
Jakoai <Jakoai@bitbucket.org>
2018-12-14 14:31:49 +0200

    boi

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ STOPALL = False
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 800), pygame.RESIZABLE)
-pygame.display.set_caption("AbsoluutseltMitteAgar.io")
+pygame.display.set_caption("Bazz")
 
 def pygame_event():
 	for event in pygame.event.get():
-	    if event.type == pygame.QUIT:
-	        STOPALL = True
+		if event.type == pygame.QUIT:
+			STOPALL = True
 
-	    #Resizable window
-	    if event.type == pygame.VIDEORESIZE:
-	        screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+		#Resizable window
+		if event.type == pygame.VIDEORESIZE:
+			screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
 
 while not STOPALL:
 	pygame_event()
